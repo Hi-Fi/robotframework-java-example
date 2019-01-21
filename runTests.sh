d48dbfaaf7dd607f2b5ed3f8647f1bfe4ecea1a8
Juho Saarinen <juho.saarinen@gmail.com>
2019-01-21 10:06:13 +0200

    Fixed mvnw executable name in script

--- runTests.sh
+++ runTests.sh
@@ -3,7 +3,7 @@ return_path=$1
 if [ -f $return_path/pom.xml ]; 
 then
 	cd $return_path
-	./mvnw.sh -Prun-tests-with-ride verify -Dride.argumentFile=$3 -Dride.listener=$5 -Dride.testCasesDirectory=$6
+	./mvnw -Prun-tests-with-ride verify -Dride.argumentFile=$3 -Dride.listener=$5 -Dride.testCasesDirectory=$6
 else
 	echo No pom.xml found in $return_path
 fi
